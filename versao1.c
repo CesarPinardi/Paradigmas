9f6fcbf983c6c118ba15c59f0763760eebb3b6ab
César Augusto Pinardi <cesar.pinardi@gmail.com>
2019-02-22 11:04:08 -0300

    Create versao1.c

---
+++ versao1.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 3
+
+struct aluno {
+	char nome[10];
+	int ra;
+}Aluno;
+
+struct professor{
+	char nome[10];
+	int id;
+	char disciplina[20];
+};
+
+void menu() {
+
+	printf("\t\t\tCadastro de Matriculas\n\n\n");
+	printf("\tEscolha uma das opcoes abaixo:\n\n");
+	printf("1)Cadastro Aluno\n");
+	printf("2)Cadastro Professor\n");
+	printf("3)Cadastro Disciplina\n");
+	printf("4)Matricular Aluno(s)\n");
+	printf("5)Cancelar Matricula\n");
+	printf("6)Vincular Disciplina\n");
+	printf("7)Desvincular Disciplina\n");
+	printf("8)Impressoes\n");
+	
+}
+
+void main() {
+	
+	struct aluno VetAluno[TAM];
+
+	menu();
+	int opcao;
+	printf("Opcao: ");
+	scanf("%d", &opcao);
+	switch (opcao)
+	{
+
+
+	default:
+		break;
+	}
+
+
+
+	for (int i = 1; i < TAM; i++)
+	{
+		printf("\nNome: ");
+		scanf("%s", VetAluno[i].nome);
+		printf("RA: ");
+		scanf("%d", &VetAluno[i].ra);
+		printf("\n\n------------------------\n\n");
+
+	}
+
+	for (int j = 1; j < TAM; j++)
+	{
+		printf("\nNome: %s", VetAluno[j].nome);
+		printf(" RA: %d\n", VetAluno[j].ra);
+	}
+
+	system("pause");
+}
